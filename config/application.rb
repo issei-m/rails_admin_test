0c06ecfed94fcc580cff502a7c01f151c16f1b43
Issei.M <issei.m7@gmail.com>
2023-08-01 12:47:12 +0900

    Rename app module

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require "action_view/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsAdminTest
+module RailsAdminWithWebpack
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
